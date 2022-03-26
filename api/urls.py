6e3870110f9ba599063e1cbb90d9301fe0f2a874
Poklok <vlad.narchyk@gmail.com>
2022-03-26 22:57:07 +0300

    CRUD api

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from api.viewsets import TaskListSet
-
-router = routers.DefaultRouter()
-router.register(r'task', TaskListSet)
+from api.viewsets import TaskListSet, TaskCreateSet, TaskUpdateSet, TaskDeleteSet
 
 urlpatterns = [
-    path('', include(router.urls), name='v1')
+    path('v1', TaskListSet.as_view()),
+    path('v1/create', TaskCreateSet.as_view()),
+    path('v1/<int:pk>', TaskUpdateSet.as_view()),
+    path('v1/<int:pk>/delete', TaskDeleteSet.as_view())
+
 ]
--- api/viewsets.py
+++ api/viewsets.py
@@ -1,10 +1,24 @@
-
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 
 from api.serializers import TaskSerializer
 from todo.models import Task
 
 
-class TaskListSet(viewsets.ModelViewSet):
+class TaskCreateSet(generics.CreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TaskListSet(generics.ListAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TaskUpdateSet(generics.RetrieveUpdateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TaskDeleteSet(generics.DestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
